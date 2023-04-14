cecd178ce832cc2ab9f027553828ad88c5b26506
msoumya <m.soumyaranjan@iserveu.in>
2023-04-14 19:42:50 +0530

    FInal Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,17 @@ export class AppComponent implements OnInit {
   masterDataSelect: any = [];
   selectData: any = [];
   useroptionList: boolean = true;
+  notevalue: any = '';
 
   @ViewChild(MatAutocompleteTrigger) autocomplete: MatAutocompleteTrigger;
-  @ViewChild('userName') userName:ElementRef;
-  showInputField: boolean = true;
+  @ViewChild('userName') userName: ElementRef;
+  showInputField: boolean = false;
+  showAutcompleteList: boolean = false;
+  elasticResDiv: any;
+  notevalueSTAT: any;
+  lastWord2: any;
+  lastWord1:any;
+  checkWord: boolean;
 
   constructor() { }
 
@@ -24,43 +31,71 @@ export class AppComponent implements OnInit {
     // Form Declaration
     this.user_insertForm = new FormGroup({
       user_name: new FormControl("", [Validators.required]),
+      // selectiveCols : new FormControl("", [Validators.required])
     });
     this.masterDataSelect = ["Gina Williams", "Jake Williams", "Jamie John", "John Doe", "Jeff Stewart", "Paula m. Keith"]
   }
 
   displayAssignMethod = (e: any) => {
-    if (e.target.value == '@') {
-      this.autocomplete.openPanel();
+    console.log(e.data);
+    this.notevalueSTAT = e.target.value;
+    console.log(this.notevalue);
+    this.showInputField = false;
+    if (e.data == '@') {
+      // this.autocomplete.openPanel();
+      this.showAutcompleteList = true;
+      this.checkWord = true;
+      this.notevalue = this.notevalueSTAT.replace(/@/g,'');
     }
     else {
-      this.autocomplete.closePanel();
+      // this.autocomplete.closePanel();
+      this.showAutcompleteList = false;
+      this.checkWord = false;
+      this.notevalue = this.notevalueSTAT;
     }
+    console.log(this.notevalue);
+
   };
+
+
+
   eSelectUser(data: any) {
     console.log(data);
+    this.showAutcompleteList = false;
     if (!this.selectData.includes(data)) {
       this.selectData.push(data);
     }
-    console.log(this.selectData);
-    this.user_insertForm.reset();
+    this.elasticResDiv = data;    
+    this.showInputField = true;
+    this.notevalue += data.replace(new RegExp(this.notevalue, "gi"), (match: string) => {
+      return match;
+    });
+    // console.log(this.notevalue);
+    this.lastWord2 = this.notevalue.split(' ').pop();
+    console.log(data.includes(this.lastWord2));
+    if (data.includes(this.lastWord2) == true) {
+      this.checkWord = true;
+      
+    }
   }
 
   // remove_chip
   removeVal(i: any) {
-    const index = this.selectData.indexOf(i);    
+    const index = this.selectData.indexOf(i);
     if (index > -1) {
       this.selectData.splice(index, 1);
     }
     console.log(this.selectData);
   }
-  
+
   // reset Button
   resetTextarea() {
     this.selectData = [];
+    this.user_insertForm.reset();
   }
   // Add Note
   add_coloum = () => {
-    this.showInputField = true;
+    // this.showInputField = true;
     setTimeout(() => this.userName.nativeElement.focus());
   }
 
